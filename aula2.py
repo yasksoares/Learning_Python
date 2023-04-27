6386d21eadb23541733377c84e700828672d92c6
Yasmim <127799113+yasksoares@users.noreply.github.com>
2023-04-27 14:06:10 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,12 @@
+print(12, 34, 56) #argumentos não nomeados
+print(78, 910, 11) #argumentos não nomeados
+
+print(12, 34, 56, sep='-') #argumentos nomeados
+print(78, 910, 11, sep="-") #argumentos nomeados
+
+print(12, 34, sep='JOAO') #argumentos nomeados
+print(78, 910, sep="MARIA") #argumentos nomeados
+
+print(12, 34, end = "##") #argumentos nomeados
+
+# o python diferencia letras maiúsculas e minúsculas
