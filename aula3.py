6343fd79ffc32e3c72a95221537ff2206124e12d
Yasmim <127799113+yasksoares@users.noreply.github.com>
2023-04-27 14:06:39 -0300

    Create aula3.py

---
+++ aula3.py
@@ -0,0 +1,26 @@
+"""
+Python = linguagem de programação
+Tipo de tipagem = Dinâmiica / forte (sabe o tipo de informação que está sendo passado para ele)
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+#Aspas simples
+print('Yasmim Kamile')
+
+#Aspas duplas
+print("A Terra é azul")
+
+#Escape 
+print("A Terra \"é azul\"")
+
+#r
+print(r'Yasmim \Kamile\'')
+
+#---------------------------------
+
+print(1,'Yasmim "Kamile"')
+print(2, "Yasmim 'Kamile'")
+
