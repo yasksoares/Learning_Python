b6246f01411df372579dadc807ca3aba8b0cae25
Yasmim <127799113+yasksoares@users.noreply.github.com>
2023-05-22 12:09:57 -0300

    exercício_IMC.py

---
+++ exercício_IMC.py
@@ -0,0 +1,10 @@
+nome = 'Yasmim Soares'
+altura = 1.74
+peso = 72
+imc = peso // (altura ** 2)
+
+# IMC = peso / altura²
+# variável = ... ( isso retornará Ellypses)
+
+print(nome, 'tem', altura, 'de altura, pesa', peso, 'quilos e o seu IMC é', imc)
+
