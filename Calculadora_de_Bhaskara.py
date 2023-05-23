bc671e21e24ba546de4dd230821297ade880bcc5
Yasmim <127799113+yasksoares@users.noreply.github.com>
2023-05-23 15:50:42 -0300

    Calculadora_de_Bhaskara.py

---
+++ Calculadora_de_Bhaskara.py
@@ -0,0 +1,16 @@
+# Calcular usando Bhaskara
+
+A = float(input('Digite o valor de a: ')) 
+B = float(input('\nDigite o valor de b: '))
+C = float(input('\nDigite o valor de c: '))
+delta = float((B **2) - (4*A*C))
+
+if delta < 0:
+    print('A equação não possui resultado real.')
+elif A == 0:
+    print('Valor indeterminado')
+else:
+    x1 = (-B + (delta ** 0.5))/(2 * A)
+    x2 = (-B - (delta ** 0.5))/(2 * A)
+    print(f"\nO valor de x' é: {x1:.3f}\n")
+    print(f"O valor de x'' é: {x2:.3f}")
