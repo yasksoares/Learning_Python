1479914126fcfb4cc360542313b2a5efabe83a97
yasksoares <yasmimkamilesoares@gmail.com>
2023-06-08 16:02:24 -0300

    Interpolacao.py

---
+++ Interpolacao.py
@@ -0,0 +1,15 @@
+""""
+# s - string
+# d e i - int
+# f - float 
+# x e X - Hexadecimal (0123456789ABCDEF)
+"""
+
+# name = 'Yasmim'
+# price = 1000.98897643
+# variable = '%s, the price is $%.2f' %(name, price)
+# print(variable)
+
+print('The hexadecimal of %d is %x' %(15,15))
+
+# we changed the name to %s and the price to %f
