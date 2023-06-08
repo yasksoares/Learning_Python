ef9e4f3be40cb9620392e5315192351af799fbd8
yasksoares <yasmimkamilesoares@gmail.com>
2023-06-08 17:05:30 -0300

    Learning the use of interpolation.

--- Interpolacao.py
+++ Interpolacao.py
@@ -5,11 +5,10 @@
 # x e X - Hexadecimal (0123456789ABCDEF)
 """
 
-# name = 'Yasmim'
-# price = 1000.98897643
-# variable = '%s, the price is $%.2f' %(name, price)
-# print(variable)
-
-print('The hexadecimal of %d is %x' %(15,15))
+name = 'Yasmim'
+price = 1000.98897643
+variable = '%s, the price is $%.2f' %(name, price)
+print(variable)
 
 # we changed the name to %s and the price to %f
+print('The hexadecimal of %d is %x' % (15,15))
